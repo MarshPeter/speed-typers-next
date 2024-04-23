f50a6ea041a45710c7ab7cac90bb67f4a55e1e6c
MarshPeter <peter.m.farmer.204@gmail.com>
2024-04-23 23:23:42 +1000

    adjusted drizzle runtime to nodejs (over node)

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -3,7 +3,7 @@ import { usersTable } from "@/app/db/schema";
 import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic"; // static by default, unless reading the request
-export const runtime = "node"; // specify the runtime to be node
+export const runtime = "nodejs"; // specify the runtime to be node
 
 export async function GET(request: Request) {
     const users = await db.select().from(usersTable);
