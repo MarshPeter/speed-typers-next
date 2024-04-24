993a5f7e26d933404fc86ba2f3afaf103d6a9ac9
MarshPeter <peter.m.farmer.204@gmail.com>
2024-04-25 02:14:49 +1000

    cleaned up header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,36 +1,51 @@
 import Link from "next/link";
-import { SignedIn, SignedOut } from "@clerk/nextjs";
-import { useEffect, useState } from "react";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Header() {
-    // const [loggedIn, setLoggedIn] = useState(false);
-
-    // useEffect(() => {
-    //     const loggedInLocally = localStorage.getItem("username");
-
-    //     if (loggedInLocally) {
-    //         setLoggedIn(true);
-    //     } else {
-    //         setLoggedIn(false);
-    //     }
-    // }, []);
-
     return (
-        <header className="flex justify-center bg-black p-3 mb-8 text-2xl text-white border-b-2 border-white">
+        <header className="flex justify-center bg-black p-3 text-2xl text-white border-b-2 border-gray-300">
             <div className="w-10/12 flex justify-between">
                 <div>
-                    <p className="font-bold">SpeedTypers</p>
+                    <Link href="/" className="font-bold">
+                        SpeedTypers
+                    </Link>
                 </div>
                 <div>
-                    <nav className="flex gap-4">
-                        <Link href="/">Home</Link>
-                        <Link href="/prompt">Race</Link>
+                    <nav className="flex gap-8">
+                        <Link
+                            className="border-b-2 border-black hover:border-gray-600"
+                            href="/"
+                        >
+                            Home
+                        </Link>
+                        <Link
+                            className="border-b-2 border-black hover:border-gray-600"
+                            href="/prompter"
+                        >
+                            Race
+                        </Link>
                         <SignedOut>
-                            <Link href="/sign-in">Sign In</Link>
-                            <Link href="/sign-up">Sign Up</Link>
+                            <Link
+                                className="border-b-2 border-black hover:border-gray-600"
+                                href="/sign-in"
+                            >
+                                Sign In
+                            </Link>
+                            <Link
+                                className="border-b-2 border-black hover:border-gray-600"
+                                href="/sign-up"
+                            >
+                                Sign Up
+                            </Link>
                         </SignedOut>
                         <SignedIn>
-                            <Link href="/profile">Profile</Link>
+                            <Link
+                                className="border-b-2 border-black hover:border-gray-600"
+                                href="/profile"
+                            >
+                                Profile
+                            </Link>
+                            <UserButton></UserButton>
                         </SignedIn>
                     </nav>
                 </div>
