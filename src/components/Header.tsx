014246660e96d11ce600c3905f45373b20bae74d
MarshPeter <peter.m.farmer.204@gmail.com>
2024-04-25 01:48:43 +1000

    added clerk and setup signin and signup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,38 +1,37 @@
-"use client";
 import Link from "next/link";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import { useEffect, useState } from "react";
 
 export default function Header() {
-    const [loggedIn, setLoggedIn] = useState(false);
+    // const [loggedIn, setLoggedIn] = useState(false);
 
-    useEffect(() => {
-        const loggedInLocally = localStorage.getItem("username");
+    // useEffect(() => {
+    //     const loggedInLocally = localStorage.getItem("username");
 
-        if (loggedInLocally) {
-            setLoggedIn(true);
-        } else {
-            setLoggedIn(false);
-        }
-    }, []);
+    //     if (loggedInLocally) {
+    //         setLoggedIn(true);
+    //     } else {
+    //         setLoggedIn(false);
+    //     }
+    // }, []);
 
     return (
-        <header className="flex justify-center bg-black p-3 mb-8 text-4xl text-white border-b-2 border-white">
+        <header className="flex justify-center bg-black p-3 mb-8 text-2xl text-white border-b-2 border-white">
             <div className="w-10/12 flex justify-between">
                 <div>
-                    <p>SpeedTypers</p>
+                    <p className="font-bold">SpeedTypers</p>
                 </div>
                 <div>
                     <nav className="flex gap-4">
                         <Link href="/">Home</Link>
                         <Link href="/prompt">Race</Link>
-                        {!loggedIn ? (
-                            <Link href="/login">Login</Link>
-                        ) : (
-                            <>
-                                <Link href="/logout">Logout</Link>
-                                <Link href="/profile">Profile</Link>
-                            </>
-                        )}
+                        <SignedOut>
+                            <Link href="/sign-in">Sign In</Link>
+                            <Link href="/sign-up">Sign Up</Link>
+                        </SignedOut>
+                        <SignedIn>
+                            <Link href="/profile">Profile</Link>
+                        </SignedIn>
                     </nav>
                 </div>
             </div>
