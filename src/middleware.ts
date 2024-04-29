c7e7f3500344aaec7582f6ba58635a7cb40b6fa5
MarshPeter <peter.m.farmer.204@gmail.com>
2024-04-29 15:11:04 +1000

    changes to clerk webhooks

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,6 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 export default clerkMiddleware();
+
 export const config = {
     matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
 };
